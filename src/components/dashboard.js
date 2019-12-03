997ea2bf605e482e7e57f54af456bf6e6534fe37
Girish Br <girishpacchu1427@gmail.com>
2019-12-03 18:26:21 +0530

    [Girish Br]Added. Navigation Menu added

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,160 +1,182 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import EditIcon from '@material-ui/icons/Edit';
-import RemoveIcon from '@material-ui/icons/Remove';
-import ArchiveIcon from '@material-ui/icons/Archive';
-import PinDropIcon from '@material-ui/icons/PinDrop';
-import LabelIcon from '@material-ui/icons/Label';
-import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/Add';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import React  from 'react'
+import jwt_decode from 'jwt-decode'
+import { Container, Card, Grid, Button, AppBar, Toolbar, CssBaseline, Divider, Drawer, Hidden, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-const drawerWidth = 240;
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
-
-function ResponsiveDrawer(props) {
-  const { container } = props;
-  const classes = useStyles();
-  const theme = useTheme();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
+ class dashboard extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      firstName: '',
+      lastName: '',
+      mobileOpen: false
+    }
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+  componentDidMount() {
+    console.log("hhhjjj")
+    const token = localStorage.usertoken
+    const decoded = jwt_decode(token)
+    this.setState({
+      email: decoded.email,
+      firstName: decoded.first_name,
+      lastName: decoded.last_name
+    })
+  }
+  handleDrawerToggle = () => {
+    this.setState({ "mobileOpen": !this.state.mobileOpen });
   };
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.history.push(`/login`)
+  }
 
-  const drawer = (
-    <div>
-      <div className={classes.toolbar} />
-      <Divider />
-      <List>
-          <ListItem button key='Add Notes'>
-            <ListItemIcon><AddIcon /></ListItemIcon>
-            <ListItemText primary='Add Notes' />
-          </ListItem>
-            <ListItem button key='Edit'>
-            <ListItemIcon><EditIcon /></ListItemIcon>
-            <ListItemText primary='Edit' />
-          </ListItem>
-            <ListItem button key='Remove'>
-            <ListItemIcon><RemoveIcon /></ListItemIcon>
-            <ListItemText primary='Remove' />
-          </ListItem>
-            <ListItem button key='Archive'>
-            <ListItemIcon><ArchiveIcon /></ListItemIcon>
-            <ListItemText primary='Archive' />
-          </ListItem>
-            <ListItem button key='Pin'>
-            <ListItemIcon><PinDropIcon /></ListItemIcon>
-            <ListItemText primary='Pin' />
-          </ListItem>
-            <ListItem button key='Label'>
-            <ListItemIcon><LabelIcon /></ListItemIcon>
-            <ListItemText primary='Label' />
-          </ListItem>
-      </List>
-    </div>
-  );
-
-  return (
-    <div className={classes.root}>
+  render() {
+    return (
+      <div className="root">
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
+      <AppBar position="fixed" className="appBar">
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
+            onClick={this.handleDrawerToggle}
+            className="menuButton"
           >
-            <MenuIcon />
+          <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Responsive drawer
+            Fundoonote
           </Typography>
         </Toolbar>
       </AppBar>
-      <nav className={classes.drawer} aria-label="mailbox folders">
+      <nav className="drawer" aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation="css">
           <Drawer
-            container={container}
+            container={this.container}
             variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
+            anchor="left"
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes="drawerPaper"
             ModalProps={{
               keepMounted: true, // Better open performance on mobile.
             }}
           >
-            {drawer}
+            <div>
+              <div className="toolbar" />
+              <Divider />
+              <List>
+                  <ListItem button key="Add Notes">
+                    <ListItemIcon> <InboxIcon /></ListItemIcon>
+                    <ListItemText primary="Add Notes" />
+                  </ListItem>
+                  <ListItem button key="Remove">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Remove" />
+                  </ListItem>
+                  <ListItem button key="Edit">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Edit" />
+                  </ListItem>
+                  <ListItem button key="Archive">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Archive" />
+                  </ListItem>
+                  <ListItem button key="Pin">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Pin" />
+                    <ListItem button key="Label">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Label" />
+                  </ListItem>
+                  </ListItem>
+              </List>
+              <Divider />
+            </div>
           </Drawer>
         </Hidden>
         <Hidden xsDown implementation="css">
           <Drawer
             classes={{
-              paper: classes.drawerPaper,
-            }}
+              paper: "drawerPaper"}}
             variant="permanent"
-            open
-          >
-            {drawer}
+            open>
+            <div>
+              <div className="toolbar" />
+              <Divider />
+              <List>
+                  <ListItem button key="Add Notes">
+                    <ListItemIcon> <InboxIcon /></ListItemIcon>
+                    <ListItemText primary="Add Notes" />
+                  </ListItem>
+                  <ListItem button key="Remove">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Remove" />
+                  </ListItem>
+                  <ListItem button key="Edit">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Edit" />
+                  </ListItem>
+                  <ListItem button key="Archive">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Archive" />
+                  </ListItem>
+                  <ListItem button key="Pin">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Pin" />
+                    </ListItem>
+                    <ListItem button key="Label">
+                    <ListItemIcon> <MailIcon /></ListItemIcon>
+                    <ListItemText primary="Label" />
+                   </ListItem>
+              </List>
+            </div>
           </Drawer>
         </Hidden>
       </nav>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
+      <main className="content">
+        <div className="toolbar" />
         <Typography paragraph>
-          
-        </Typography>
-        <Typography paragraph>
-         
+          <Card class="profileCard">
+            <Container component="main" maxWidth="xs">
+              <div>
+                <Typography component="h1" variant="h5">
+                  Profile
+                </Typography>
+                <div>
+                  First Name:{this.state.firstName}
+                </div>
+                <div>
+                  last Name:{this.state.lastName}
+                </div>
+                <div>
+                  Email : {this.state.email}
+                </div>
+                <Grid container>
+                  <Grid item>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      onClick={this.onSubmit}
+                    >
+                      Logout
+              </Button>
+                  </Grid>
+                </Grid>
+              </div>
+            </Container>
+          </Card>
         </Typography>
       </main>
     </div>
-  );
+    )
+  }
 }
-export default ResponsiveDrawer;
+export default dashboard;
