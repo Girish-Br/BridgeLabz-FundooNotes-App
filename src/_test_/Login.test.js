078dfa81aa25f7dccc5de2581dcc2dbbbb7aa34a
Girish Br <girishpacchu1427@gmail.com>
2019-12-05 18:48:52 +0530

    [Girish Br]Added. Tdd with jest and enzyme

--- src/_test_/Login.test.js
+++ src/_test_/Login.test.js
@@ -1,54 +1,48 @@
 import React from 'react';
 import {
-  shallow
+    shallow
 } from 'enzyme';
-import Login from '../screens/login';
-import '../setupTest'
+import Login from '../components/login';
+import '../setUpTest'
 /**
  * describe what we are testing
  **/
 describe('Login Component', () => {
-  /**
-   * make our assertion and what we expect to happen 
-   **/
-  it('should render without throwing an error', () => {
-    expect(shallow( < Login / > ).exists()).toBe(true)
-  })
-  /**
-   * within the Login components describe function
-   **/
-  it('renders a email input', () => {
-    expect(shallow( < Login / > ).find('#email').length).toEqual(1)
-  })
-  it('renders a password input', () => {
-    expect(shallow( < Login / > ).find('#password').length).toEqual(1)
-  })
-  /**
-   * within the Login components describe function
-   **/
-  describe('Email input', () => {
+    /**
+     * make our assertion and what we expect to happen 
+     **/
+    it('should render without throwing an error', () => {
+        expect(shallow(< Login />).exists()).toBe(true)
+    })
+    it('renders a email input', () => {
+        expect(shallow( < Login /> ).find('#outlined-email-input').length).toEqual(1)
+      })
+      it('renders a password input', () => {
+        expect(shallow( < Login /> ).find('#outlined-pass-input').length).toEqual(1)
+      })
+})
+describe('Email input', () => {
     it('should respond to change event and change the state of the Login Component', () => {
-      const wrapper = shallow( < Login / > );
-      wrapper.find('#email').simulate('change', {
+      const wrapper = shallow( < Login /> );
+      wrapper.find('#outlined-email-input').simulate('change', {
         target: {
           name: 'email',
-          value: 'hithu@gmail.com'
+          value: 'girishpacchu1427@gmail.com'
         }
       });
-      expect(wrapper.state('email')).toEqual('hithu@gmail.com');
+      expect(wrapper.state('email')).toEqual('girishpacchu@gmail.com');
     })
   })
   describe('Password input', () => {
     it('should respond to change event and change the state of the Login Component', () => {
-      const wrapper = shallow( < Login / > );
-      wrapper.find('#password')
+      const wrapper = shallow( < Login /> );
+      wrapper.find('#outlined-pass-input')
         .simulate('change', {
           target: {
             name: 'password',
-            value: 'rockz'
+            value: 'giri141519'
           }
         });
       expect(wrapper.state('password')).toEqual('rockz');
     })
   })
-})
