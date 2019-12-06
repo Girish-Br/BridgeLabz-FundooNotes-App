0ec8b22296e974df55ed33d275ab3484e77c9cdb
Girish Br <girishpacchu1427@gmail.com>
2019-12-06 09:37:21 +0530

    [Girish Br]Refactor. Added email validation for forgot password page

--- src/components/forgotpassword.js
+++ src/components/forgotpassword.js
@@ -7,13 +7,15 @@
  *  @since          : 1-12-2019
  *****************************************************************************************/
 import React from 'react';
-import { Card,Button,TextField } from '@material-ui/core';
-import {forgotpassword} from '../controller/userController';
+import { Card, Button, TextField, Snackbar, IconButton } from '@material-ui/core';
+import { forgotpassword } from '../controller/userController';
 export default class ForgotPwd extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: ''
+      email: '',
+      snackbarOpen: false,
+      snackbarMsg: ''
     }
   }
   handleClick = () => {
@@ -26,32 +28,43 @@ export default class ForgotPwd extends React.Component {
         snackbarMsg: "Invalid email..!"
       });
     }
-    else{
-    forgotpassword(this.state.email).then(res => {
-      if (res === 'success') {
-        this.setState({
-          snackbarOpen: true,
-          snackbarMessage: 'rest password link sent to your email'
-        })
-      }
-      else {
-        this.setState({
-          snackbarOpen: true,
-          snackbarMsg: res
-          
-        })
-      }
-    })
+    else {
+      forgotpassword(this.state.email).then(res => {
+        if (res === 'success') {
+          this.setState({
+            snackbarOpen: true,
+            snackbarMsg: "rest password link sent to your email"
+          })
+        }
+        else {
+          this.setState({
+            snackbarOpen: true,
+            snackbarMsg: res
+          })
+        }
+      })
+    }
   }
-}
+  //used to display brief message
+  snackbarClose = (e) => {
+    this.setState({ snackbarOpen: false });
+  }
   onChangeEmail = (e) => {
-    this.setState({[e.target.name]:e.target.value})
+    this.setState({ [e.target.name]: e.target.value })
   }
   render() {
     return (
       <form className="forgotpwd">
         <Card className="card">
           <h1 className="forgotpwd_head">Enter email</h1>
+          <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center', }}
+            open={this.state.snackbarOpen}
+            autoHideDuration={6000}
+            onClose={this.snackbarClose}
+            message={<span id="messege-id">{this.state.snackbarMsg}</span>}
+            action={
+              <IconButton key="close" arial-label="close" color="inherit" onClick={this.snackbarClose}>
+              </IconButton>} />
           <div>
             <TextField
               id="email"
